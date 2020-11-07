da1e5cf6be380b3a6a6654cf64f0c026b8908ea1
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-11-07 17:23:59 +0530

    fin1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
+const contacts = [
+  {
+    phone: "18602100000",
+    email: "guestcaredominos@jublfood.com"
+  }
+];
 
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.status(200).send("Welcome to Dominos!");
+    return;
+  } else if (req.url === "/contact") {
+    res.status(200).send(contacts);
+    return;
+  } else {
+    res.status(400);
+  }
 }
 
+httpServer.listen(8081);
 module.exports = httpServer;
